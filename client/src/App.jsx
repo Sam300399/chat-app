f0b70570d5309cbe9a17c251edb7f914a8e0d4af
Sameer Shaik <163137363+Sam300399@users.noreply.github.com>
2025-07-20 12:15:36 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("https://chat-app-c6et.onrender.com");
+const socket = io("https://chat-app-3v0o.onrender.com");
 const ROOM_ID = "demo-room"; // hard‑coded; you can make this dynamic
 
 export default function App() {
