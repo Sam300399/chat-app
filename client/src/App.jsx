c5d7aa3b9cf7b7e5dea3eac8cc6c1350739a5642
Sameer Shaik <163137363+Sam300399@users.noreply.github.com>
2025-07-20 11:05:40 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,11 @@ export default function App() {
 
       // Create peer connection
       const pc = new RTCPeerConnection({
-        iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+        iceServers: [{ urls: "stun:stun.l.google.com:19302" },{
+      urls: 'turn:openrelay.metered.ca:80',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    }],
       });
       pcRef.current = pc;
 
